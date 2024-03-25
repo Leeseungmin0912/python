a21518b2c77b8304c9a945d24033cd8170801fb7
Leeseungmin0912 <155872533+Leeseungmin0912@users.noreply.github.com>
2024-03-25 10:53:02 +0900

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,37 @@
+#연산자
+#1. 산술연산자 + - * / % // **
+#2. 대입연산자 = += -= *= /=
+'''
+age = 17 #17을 age라는 변수에 대입(넣어준다)
+print("age 변수의 값:", age) #17
+after_2 = 2
+print(age + after_2) #19
+print(age - after_2) #15
+result = age - after_2
+print("결과:",result)
+#prit('결과:', age - after-2)이렇게 써도 작동은 함 대신 비효율 적으로 코드를 작성하는 것임.
+'''
+
+
+age =  18
+print("age 변수의 값:")   #20
+age += 2     #age = age + 2  답은 20이 출력된다.
+print("age변수의 값:",age)
+age -=2 #age = age -2
+print('age변수의 값:',age)
+age += 2 # age = age *2
+print('age변수의 값:',age)
+age /= 2 #age = age /2
+print('age변수의 값:',age)
+age //= 2 #age = age //2
+print('age변수의 값:',age)
+age %= 2 #age = age % 2
+print('age변수의 값:',age)
+age **= 2 #age = age ** 2
+print('age변수의 값:',age)
+
+
+
+
+
+
