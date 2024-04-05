dfc05c2823c6766c9eb2ec91e16399ced8662cd4
Leeseungmin0912 <155872533+Leeseungmin0912@users.noreply.github.com>
2024-04-05 14:34:16 +0900

    Add files via upload

---
+++ str_test.py
@@ -0,0 +1,48 @@
+#변수 --- 자료를 담는 공간
+'''
+에약어 ( if for in Ture False ···․)
+
+'''
+#함수 --- 자주 사용하는 기능
+'''
+내장함수 ( print Type ···.)
+'''
+#연산자
+'''
+산술 연산자 9 + - * /), 대입 연산자( = ) , 논리 연산자(and or not) , 비교 연산자(>,<)(관계연산자) ··· .)
+'''
+#자료형
+'''
+int(정수) , 실수(float) , 문자(str)
+'''
+#이스케이스코드 --- 문자열 내에서 특별하게 쓰이는 코드
+'''
+ex) 줄바꿈 \n
+'''
+#문자열 다루기
+'''
+인덱싱
+'''
+
+n = "happy programming!"
+print(n)
+m = n[0:5]
+print(m)
+print(n[-1])
+print(n[-4:-1])
+print(n[-12:-8])
+print(n[6:10])
+
+
+
+n = "happy programming!"
+print(len(n)) # len은 글자수를  세어주는 명령어다.
+print(n.upper()) # upper()은 모두 대문자로 바꿔주는 내장함수이다.
+name = '제 이름은 `이승민입니다'
+print(len(name))
+
+name_e = 'my name is seungmin'
+print(len(name_e))
+print(name_e.upper())
+print(name_e.lower())
+print(name_e.capitalize())
